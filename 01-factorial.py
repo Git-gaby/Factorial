fdd772dd23d9fc73db229dde9de289ff7914af8b
Gabriel <gabriel0322024@gmail.com>
2025-08-11 14:43:07 -0500

    factorial solucionado

--- 01-factorial.py
+++ 01-factorial.py
@@ -1,1 +1,10 @@
-print("Hola Git")
+# Pedimos el número al usuario
+numero = int(input("Ingresa un número entero: "))
+
+factorial = 1  # Valor inicial
+
+# Bucle para calcular el factorial
+for i in range(1, numero + 1):
+    factorial *= i  # equivalente a factorial = factorial * i
+
+print(f"El factorial de {numero} es {factorial}")
